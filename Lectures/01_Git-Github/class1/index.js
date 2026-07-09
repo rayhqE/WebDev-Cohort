9cb4f92cab39c164b4587881beae42a573ce67bc
Rayyan <rayhq3@gmail.com>
2026-07-09 20:32:37 +0530

    I have just merged changes from feat a branch

--- Lectures/01_Git-Github/class1/index.js
+++ Lectures/01_Git-Github/class1/index.js
@@ -1,9 +1,32 @@
 function add(a, b) {
   return a + b;
 }
+function sub(a, b) {
+  if (!(a > b)) throw new Error("num a must be greater than b ");
+  return a - b;
+}
 
 add(3, 4);
 add(3, 4);
+
+sub(10, 3);
 
 //always leave a single line at last before commit
 //always write commit message in the present tense
+//git log --oneline
+//git diff <hash> <hash>
+//git status
+//git add .
+//git commit -m "msg"
+//git remote set-url origin <url>
+
+//<<Fork a repo then make some changes in it then open a pull request to the base repo ...either will be merged  or closed >> open source contribution 101 XD
+//
+//
+//
+//
+//
+//
+//
+//
+//
