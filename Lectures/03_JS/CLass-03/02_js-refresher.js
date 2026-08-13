26962095846f38da643e8b406996f7a12c4d4efe
Rayyan <rayhq3@gmail.com>
2026-08-13 11:38:22 +0530

    JS Class-3 Done

---
+++ Lectures/03_JS/CLass-03/02_js-refresher.js
@@ -0,0 +1,32 @@
+//console.log("Hello World");
+let favLang;
+myFavLang = "javascript";
+//console.log(favLang);
+let myNum = -45.56;
+//console.log(myNum);
+//console.log(-1 / 0);
+//console.log(6 / "not a num");
+let single = "this is single";
+let double = "this is double";
+let backtick = `This is String Interpolation`;
+let name = "Rayyan";
+//console.log(`My Name is ${name}`);
+function sumThis(a, b) {
+  return a + b;
+}
+//console.log(`Sum of 4 and 5 is ${sumThis(4, 5)}`);
+
+let isItRaining = true;
+let canWeGoOut = false;
+
+let partner = null;
+let ekVariableHai;
+//console.log(ekVariableHai);
+
+//console.log(typeof console.log);
+let counter = 10;
+console.log(++counter);
+console.log(counter);
+if (age >= 12) {
+  console.log("adult");
+}
